229ec1fb90e0eb36730f60abba75f4aa15c26ade
SerjoGG <golovnyamagadan@gmail.com>
2024-12-04 02:12:34 +1100

    обновление

--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,13 +1,17 @@
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box, Paper } from '@mui/material';
 import { User } from '../types';
-
 export default function HomePage({ user }: { user: User }) {
   return (
     <Container maxWidth="lg">
       <Box sx={{ mt: 4, mb: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          WildGuide - ВСЕМ ПРИВЕТ. ТУТ Я ХОЧУ ПАЛАРАКС
+          WildGuide
         </Typography>
+        <Paper elevation={3} sx={{ p: 3 }}>
+          <Typography variant="body1">
+            Добро пожаловать в WildGuide - ваш помощник для охоты и активного отдыха
+          </Typography>
+        </Paper>
       </Box>
     </Container>
   );
