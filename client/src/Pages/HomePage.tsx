78f8f731ddbe35a5af2f76f745a39bd9a78f3df6
Evgeny <heaneypower@gmail.com>
2024-12-04 14:05:03 +0300

    фикс паралакс(пока не готовый)

--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,54 +1,55 @@
-import { motion, useScroll, useTransform } from 'framer-motion';
-import styles from '../Styles/HomePage.module.css';
+import { motion, useScroll, useTransform } from "framer-motion";
+import styles from "../Styles/HomePage.module.css";
 
 const HomePage: React.FC = () => {
-  const { scrollY } = useScroll();
+  const { scrollYProgress } = useScroll();
 
-  const baseY = useTransform(scrollY, [0, 1000], [0, 100]);
-  const middleY = useTransform(scrollY, [0, 1000], [0, 200]);
-  const frontY = useTransform(scrollY, [0, 1000], [0, 300]);
+  const baseY = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const middleY = useTransform(scrollYProgress, [0, 1], [0, -200]);
+  const frontY = useTransform(scrollYProgress, [0, 1], [0, -300]);
 
   return (
     <div className={styles.wrapper}>
       <header className={styles.mainHeader}>
-          <h1 className={styles.layersTitle}>Wild Guide</h1>
+        <h1 className={styles.layersTitle}>Wild Guide</h1>
       </header>
-      
+
       <div className={styles.layers}>
         <motion.div
           className={`${styles.layer} ${styles.layersBase}`}
           style={{
-            backgroundImage: 'url(../img/layer-base.png)',
+            backgroundImage: `url(/img/layer-base.png)`,
             y: baseY,
           }}
         />
         <motion.div
           className={`${styles.layer} ${styles.layersMiddle}`}
           style={{
-            backgroundImage: 'url(../img/layer-middle.png)',
+            backgroundImage: `url(/img/layer-middle.png)`,
             y: middleY,
           }}
         />
         <motion.div
           className={`${styles.layer} ${styles.layersFront}`}
           style={{
-            backgroundImage: 'url(../img/layer-front.png)',
+            backgroundImage: `url(/img/layer-front.png)`,
             y: frontY,
           }}
         />
       </div>
 
-        
-        <div className={styles.layers}>
+      <div className={styles.ground} />
+
+      <div className={styles.dungeonWrapper}>
         <h2>Welcome to the Map</h2>
         <motion.div
           className={`${styles.layer} ${styles.dungeon}`}
           style={{
-            backgroundImage: 'url(../img/dungeon.png)',
+            backgroundImage: `url(/img/dungeon.png)`,
             y: baseY,
           }}
         />
-        </div>
+      </div>
     </div>
   );
 };
