cede32f065b98f9d69ee2059a21ad0fb1a65621b
SerjoGG <golovnyamagadan@gmail.com>
2024-12-07 09:02:20 +1100

    добавление компонентов

--- client/src/components/MapPage/YandexMap/HuntingAreasLayer.tsx
+++ client/src/components/MapPage/YandexMap/HuntingAreasLayer.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Polygon } from '@pbe/react-yandex-maps';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../../../Redux/index';
+import { fetchHuntingAreas } from '../../../Redux/Thunks/MapPage/huntingAreasThunks';
+import { selectAllAreas, selectArea } from '../../../Redux/Slices/MapPage/huntingAreasSlice';
+import { HuntingArea } from '../../../types/MapPage/HuntingAreaType';
 
 interface HuntingAreasLayerProps {
   visible: boolean;
 }
 
 export const HuntingAreasLayer: React.FC<HuntingAreasLayerProps> = ({ visible }) => {
-  if (!visible) return null;
+  const dispatch = useDispatch<AppDispatch>();
+  const areas = useSelector(selectAllAreas);
+  
+  useEffect(() => {
+    dispatch(fetchHuntingAreas());
+  }, [dispatch]);
+  
+  const handlePolygonClick = (e: any, area: HuntingArea) => {
+    e.preventDefault();
+    dispatch(selectArea(area));
+  };
+
+  if (!visible || !areas || !Array.isArray(areas)) return null;
   
   return (
     <>
-      {/* Полигоны охотничьих угодий */}
+      {areas.map((area) => (
+        <Polygon
+          key={area.id}
+          geometry={area.coordinates}
+          options={{
+            fillColor: 'rgba(0, 255, 0, 0.3)',
+            strokeColor: '#00FF00',
+            strokeWidth: 2
+          }}
+          onClick={(e) => handlePolygonClick(e, area)}
+        />
+      ))}
     </>
   );
 };
-
-
-
-HuntingAreasLayer.tsx
